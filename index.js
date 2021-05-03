11292cc2b2aa8e0d1993b9ab102e6a067e258f57
o-tenyo <johnstone.omundi@student.moringaschool.comgit config --global user.email johnstone.omundi@student.moringaschool.com>
2021-05-03 19:11:25 +0300

    Added a second function to my JS script and an image file to my repo

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
+var dailyCapacity = 0;
+var sellingPrice = 45; //per litre
+var productionPerShed = {
+  A : 370,
+  B : 500,
+  C : 350,
+  D : 540,
+  E : 390,
+  F : 400,
+  G : 770,
+  H : 590,
+}
+
 function totalProduction(){
-    var sum = 0;
-    var productionPerShed = {
-          A : 370,
-          B : 500,
-          C : 350,
-          D : 540,
-          E : 390,
-          F : 400,
-          G : 770,
-          H : 590,
-      }
-      for (var property in productionPerShed){
-          console.log(`Your production in Shed ${property} is ${productionPerShed[property]} litres per day`);
-          sum = sum + productionPerShed[property]
-      }
-    console.log(`Total production is ${sum} litres per day`)
+  for (var property in productionPerShed){
+    console.log(`Your production in Shed ${property} is ${productionPerShed[property]} litres per day`);
+    dailyCapacity = dailyCapacity + productionPerShed[property];
+}
+  console.log(`Total production is ${dailyCapacity} litres per day`);
+}
+
+totalProduction();
+
+function incomeOverTime(sellingPrice, time){
+  var dailyIncome = dailyCapacity*sellingPrice;
+  console.log(`Your daily income will be Ksh ${dailyIncome}`)
+  var weeklyIncome = dailyIncome*7;
+  console.log(`Your weekly income will be Ksh ${weeklyIncome}`)
+  var monthlyIncome = weeklyIncome*4;
+  console.log(`Your monthly income will be Ksh ${monthlyIncome}`)
+  var yearlyIncome = weeklyIncome*52;
+  console.log(`Your yearly income will be Ksh ${yearlyIncome}`)
   }
-  totalProduction()
-  
+incomeOverTime(); 
